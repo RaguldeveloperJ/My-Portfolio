d238e461b339caeadebde0e8adf7301783ba7d31
RaguldeveloperJ <jkrdragul2018@gmail.com>
2025-12-11 13:24:57 +0530

    commit

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,20 @@
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/my-portfolio/', 
   plugins: [react()],
   build: {
-     chunkSizeWarningLimit: 2000,
+    chunkSizeWarningLimit: 2000,
     rollupOptions: {
       output: {
-        manualChunks: {
-          'react-vendors': ['react', 'react-dom'],
-          'emailjs': ['emailjs-com'],
+        manualChunks(id) {
+          if (id.includes('node_modules/react')) {
+            return 'react-vendors'
+          }
+          if (id.includes('node_modules/emailjs-com')) {
+            return 'emailjs'
+          }
         }
       }
     }
