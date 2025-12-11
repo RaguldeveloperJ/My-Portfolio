de624d769210ed69ea74b9b7c4dca10e857e278c
RaguldeveloperJ <jkrdragul2018@gmail.com>
2025-12-11 13:18:25 +0530

    commit

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,17 @@
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  rollupOptions: {
-      external: ['emailjs-com']
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'react-vendors': ['react', 'react-dom'],
+          'emailjs': ['emailjs-com'],
+        }
+      }
     }
+  }
 })
